379c7bfe6d1c77abf80ea790d56bdfabbe0387e4
duraanos <duran364@anadolu.edu.tr>
2025-10-16 10:30:45 +0300

    feat(user): Implement user model with schema validation and role management

---
+++ src/features/user/models/user.model.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import { User } from '../types/user';
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: (email: string) => validator.isEmail(email),
+        message: props => `${props.value} Invalid email address`,
+      },
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      validate: {
+        validator: function (v: string) {
+          return validator.matches(v, /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/);
+        },
+        message:
+          'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+      },
+    },
+
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+
+    metadata: {
+      type: Object,
+      default: {},
+    },
+  },
+  { timestamps: true }
+);
+
+export const NewUser = mongoose.model<User>('NewUser', userSchema);
