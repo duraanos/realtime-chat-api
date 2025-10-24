a854da21ae15b67402262abec7a643bfc5417dd6
duraanos <duran364@anadolu.edu.tr>
2025-10-24 16:03:11 +0300

    feat(auth): Add handleRefreshToken method for managing JWT refresh tokens

--- src/features/auth/controllers/auth.controller.ts
+++ src/features/auth/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { authService } from '../services/auth.service';
+import { clearRefreshCookie, setRefreshCookie } from '../utils/cookie';
 
 export const authController = {
   async registerUser(req: Request, res: Response): Promise<void> {
@@ -25,6 +26,30 @@ export const authController = {
       res
         .status(500)
         .json({ error: err instanceof Error ? err.message : String(err) });
+    }
+  },
+
+  async handleRefreshToken(req: Request, res: Response): Promise<void> {
+    const cookies = req.cookies;
+
+    if (!cookies?.jwt) res.status(401).json({ error: 'Unauthorized' });
+    const refreshToken = cookies.jwt;
+
+    clearRefreshCookie(res);
+
+    try {
+      const { accessToken, newRefreshToken } =
+        await authService.handleRefreshToken(refreshToken);
+
+      setRefreshCookie(res, newRefreshToken);
+
+      res.json({ accessToken });
+    } catch (err: any) {
+      const statusCode = err.message.split(':')[0];
+      if (statusCode === '403') res.status(403);
+
+      console.error('RefreshToken:', err);
+      res.sendStatus(500);
     }
   },
 };
