3d70241f96b7178ee34bc5b148b8a0374ddfcde3
duraanos <duran364@anadolu.edu.tr>
2025-10-26 15:58:24 +0300

    feat(app): Add user routes to handle user-related API requests

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ dotenv.config();
 
 import connectDb from './shared/config/db';
 import authRoutes from '../src/features/auth/routes/auth.routes';
+import userRoutes from '../src/features/user/routes/user.routes';
 
 const app = express();
 connectDb();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
