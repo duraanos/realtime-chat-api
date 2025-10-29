b76596c925f2f6a3572d863b594a7c20d44f4816
duraanos <duran364@anadolu.edu.tr>
2025-10-29 15:05:40 +0300

    feat(chat): Set up HTTP server and integrate Socket.IO for real-time communication

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
 import connectDb from './shared/config/db';
 import authRoutes from '../src/features/auth/routes/auth.routes';
+import messageRoutes from './features/chat/routes/message.routes';
+import { socketController } from './features/chat/controllers/socket.controller';
 
 const app = express();
 connectDb();
@@ -17,7 +21,20 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+io.on('connection', socketController.handleSocketConnection(io));
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
