2bb24a0f4e153292ccc4ada844b3213cf91f774a
duraanos <duran364@anadolu.edu.tr>
2025-10-29 14:56:13 +0300

    feat(chat): Implement messageService with saveMessage and getRoomMessages methods

---
+++ src/features/chat/services/message.service.ts
@@ -0,0 +1,32 @@
+import { MessageModel } from '../models/message.model';
+import { Message, MessageDocument } from '../types/custom';
+
+export const messageService = {
+  async saveMessage(
+    messageData: Omit<Message, '_id' | 'timestamp'>
+  ): Promise<MessageDocument> {
+    try {
+      const message = new MessageModel(messageData);
+      await message.save();
+
+      return message;
+    } catch (err: any) {
+      console.error('An error occurred while saving the message:', err);
+      throw new Error('Message could not be saved!');
+    }
+  },
+
+  async getRoomMessages(room: string, limit: number = 50): Promise<Message[]> {
+    try {
+      const messages = await MessageModel.find({ room })
+        .sort({ timeStamp: -1 })
+        .limit(limit)
+        .lean<Message[]>();
+
+      return messages.reverse();
+    } catch (err: any) {
+      console.error('An error occurred while retrieving messages');
+      throw new Error('Messages could not be retrieved');
+    }
+  },
+};
