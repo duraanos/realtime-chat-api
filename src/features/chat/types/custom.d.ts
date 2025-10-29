f4dbf91f7a0e796eb04efd1962c1e2deaf42eb5a
duraanos <duran364@anadolu.edu.tr>
2025-10-29 14:53:50 +0300

    feat(chat): Add types for PrivateMessage, SendPrivateMessagePayload, and enhance existing types

--- src/features/chat/types/custom.d.ts
+++ src/features/chat/types/custom.d.ts
@@ -1,3 +1,6 @@
+import mongoose from 'mongoose';
+import { Server } from 'socket.io';
+
 export type Message = {
   _id: string;
   user: string;
@@ -6,10 +9,25 @@ export type Message = {
   timestamp?: string;
 };
 
+export type PrivateMessage = {
+  sender: string;
+  receiver: string;
+  text: string;
+  timestamp?: Date;
+};
+
+export type MessageDocument = mongoose.Document & Omit<Message, '_id'>;
+
 export type SendMessagePaylod = {
   user: string;
   text: string;
   room: string;
+};
+
+export type SendPrivateMessagePaylod = {
+  senderId: string;
+  receiverId: string;
+  text: string;
 };
 
 export type JoinRoomResponse = {
@@ -18,3 +36,8 @@ export type JoinRoomResponse = {
   history?: Message[];
   message?: string;
 };
+
+export type IOServer = Server;
+export type Callback = (
+  response: JoinRoomResponse | { status: 'ok'; message?: string }
+) => void;
