eef312598f64a859f47f498c571c206bab7858f8
duraanos <duran364@anadolu.edu.tr>
2025-11-01 19:28:07 +0300

    feat(redis): Add online user management methods to redisService

--- src/features/chat/services/redis.service.ts
+++ src/features/chat/services/redis.service.ts
@@ -1,6 +1,7 @@
 import redisClient from '../config/redis';
 
 const SOCKET_ID_PREFIX = 'user:socket:';
+const ONLINE_USERS_SET_KEY = 'chat:onlineUsers';
 
 export const redisService = {
   async setUserSocket(userId: string, socketId: string): Promise<string> {
@@ -13,5 +14,17 @@ export const redisService = {
 
   async removeUserSocket(userId: string): Promise<void> {
     await redisClient.del(`${SOCKET_ID_PREFIX}${userId}`);
+  },
+
+  async addUserToOnlineSet(userId: string): Promise<void> {
+    await redisClient.sadd(ONLINE_USERS_SET_KEY, userId);
+  },
+
+  async removeUserToOnlineSet(userId: string): Promise<void> {
+    await redisClient.srem(ONLINE_USERS_SET_KEY, userId);
+  },
+
+  async getOnlineUsers(): Promise<string[]> {
+    return await redisClient.smembers(ONLINE_USERS_SET_KEY);
   },
 };
