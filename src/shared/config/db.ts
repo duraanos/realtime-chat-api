7ddab0554d65fc888696bb39dcdebe1202339680
duraanos <duran364@anadolu.edu.tr>
2025-11-01 16:17:33 +0300

    feat(db): Enhance MongoDB connection error handling and logging

--- src/shared/config/db.ts
+++ src/shared/config/db.ts
@@ -10,11 +10,24 @@ const connectDb = async (): Promise<void> => {
     console.log('MongoDB connected successfully');
   } catch (error: unknown) {
     if (error instanceof Error) {
+      console.log(error.message);
       console.error('MongoDB connection failed:', error.message);
+      process.exit(1);
     } else {
       console.error('Unknown MongoDB connection error:', error);
     }
   }
 };
 
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error event', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.warn('MongoDb disconnected event');
+});
+
+mongoose.connection.on('reconnected', () => {
+  console.log('MongoDB reconnected event ');
+});
 export default connectDb;
