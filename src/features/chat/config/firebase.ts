9efb4c2e04c27e693c195ebcd0fce6afdfe4367c
duraanos <duran364@anadolu.edu.tr>
2025-11-08 18:44:02 +0300

    feat(firebase): Add firebase configuration and initialization for FCM

---
+++ src/features/chat/config/firebase.ts
@@ -0,0 +1,16 @@
+import admin from 'firebase-admin';
+
+const firebaseCredentials = process.env.FIREBASE_CREDENTIALS;
+if (!firebaseCredentials) {
+  throw new Error('FIREBASE_CREDENTIALS environment variable is not set');
+}
+
+const serviceAccount = JSON.parse(firebaseCredentials) as admin.ServiceAccount;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export const fcmAdmin = admin;
